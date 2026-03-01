05606c24d0235078362789a7d3eea36fe4bec999
Kinopio <125463022+kinopio1101@users.noreply.github.com>
2026-03-01 20:54:51 +0800

    chore: remove deprecated files replaced by capa
    - Delete elf_parser.py (replaced by capa)
    - Delete function_classifier.py (replaced by capa)
    - Delete mitre_mapper.py (replaced by capa)
    - Delete data/linux_func_categories.json (replaced by capa rules)
    - Delete data/mitre_for_linux.json (replaced by capa rules)
    - Update __init__.py to remove deprecated exports

--- src/threatscope/analysis/tools/static/__init__.py
+++ src/threatscope/analysis/tools/static/__init__.py
@@ -2,20 +2,14 @@
 
 from src.threatscope.analysis.tools.static.capa_analyzer import CapaAnalyzer
 from src.threatscope.analysis.tools.static.diec_analyzer import DiecAnalyzer
-from src.threatscope.analysis.tools.static.elf_parser import ELFParser
-from src.threatscope.analysis.tools.static.function_classifier import FunctionClassifier
 from src.threatscope.analysis.tools.static.hash_calculator import HashCalculator
-from src.threatscope.analysis.tools.static.mitre_mapper import MitreMapper
 from src.threatscope.analysis.tools.static.string_extractor import StringExtractor
 from src.threatscope.analysis.tools.static.yara_scanner import YaraScanner
 
 __all__ = [
     "CapaAnalyzer",
     "DiecAnalyzer",
-    "ELFParser",
-    "FunctionClassifier",
     "HashCalculator",
-    "MitreMapper",
     "StringExtractor",
     "YaraScanner",
 ]
