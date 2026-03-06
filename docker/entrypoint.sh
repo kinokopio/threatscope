199e52ad305e3143f48d076aa0711206d89772f9
Kinopio <125463022+kinopio1101@users.noreply.github.com>
2026-03-06 16:18:39 +0800

    1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-if [ "$(id -u)" = "0" ]; then
-    chown -R appuser:appuser /app/data
-    exec gosu appuser "$@"
-else
-    exec "$@"
+chown -R appuser:appuser /app/data
+
+if [ -S /var/run/docker.sock ]; then
+    DOCKER_GID=$(stat -c '%g' /var/run/docker.sock)
+    if ! getent group docker > /dev/null 2>&1; then
+        groupadd -g "$DOCKER_GID" docker
+    fi
+    usermod -aG docker appuser
 fi
+
+exec gosu appuser "$@"
